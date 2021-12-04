a71496b712347e8ac5b146015116cedd35914c43
Donald Geddes <hi@donaldgeddes.ca>
2021-12-03 17:44:52 -0800

    Add regex to match torrent URL from announce message

--- src/irc/handle-message.js
+++ src/irc/handle-message.js
@@ -1,1 +1,7 @@
-export default async (_, message) => {}
+export default async (_, message) => {
+  // Parses for torrent URL
+  const re = /(?<url>https.+)\s\//
+  const {
+    groups: { url },
+  } = message.match(re)
+}
