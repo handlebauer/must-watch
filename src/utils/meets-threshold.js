18cff1e7a48bbbb2ac2b46bd48558623c39155c4
Donald Geddes <hi@donaldgeddes.ca>
2022-01-15 01:14:59 -0800

    Improve logging

--- src/utils/meets-threshold.js
+++ src/utils/meets-threshold.js
@@ -1,16 +1,20 @@
 export const meetsThreshold = movie => {
   const rating = movie.ratings[process.env.RATING_SOURCE]
-  // NOTE: rating may be undefined, in which case this will always assign as false
-  const exceedsRating = rating?.raw >= process.env.RATING_MINIMUM
-  console.log(`  => Rating: ${rating.name} - ${rating.value} `)
 
-  const voteCount = rating?.count
+  if (!rating) {
+    return false
+  }
+
+  const exceedsRating = rating.raw >= process.env.RATING_MINIMUM
+  console.log(`  => Rating: ${rating.name} - ${rating.value}`)
+
+  const voteCount = rating.count
   // NOTE: if the rating source doesn't include a vote count, this will assign as
   // true (i.e. we don't care about vote count for those sources with none)
   const exceedsVoteCount = voteCount
     ? voteCount > process.env.VOTE_MINIMUM
     : true
-  console.log(`  => Vote Count: ${rating.count ? rating.count : 'N/A'}`)
+  console.log(`  => Vote Count: ${rating.count || 'N/A'}`)
 
   return exceedsRating && exceedsVoteCount
 }
