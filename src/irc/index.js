82318fd4c3eb7fabd4127308dfe43c6352161b72
Donald Geddes <hi@donaldgeddes.ca>
2022-01-21 11:46:17 -0800

    Improve logging

--- src/irc/index.js
+++ src/irc/index.js
@@ -14,5 +14,8 @@ export default async log => {
   await log.send()
 
   const client = new Client(IRC_SERVER, BOT_NAME, config)
+  log.add(`IRC: connecting to ${IRC_SERVER}:${config.port}`)
+  await log.send()
+
   client.connect(handleConnect(client, log))
 }
